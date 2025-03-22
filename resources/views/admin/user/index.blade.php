b0aa50e5e88b882f86f40a4e6ae33e68f57ec908
NguyenDangBao <91870844+NguyenDangBao@users.noreply.github.com>
2025-03-22 11:51:58 +0700

    sửa lại index cho đúng đường dẫn

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -20,7 +20,7 @@
                             </div>
 
                             <div class="page-title-actions">
-                                <a href="./user-create.html" class="btn-shadow btn-hover-shine mr-3 btn btn-primary">
+                                <a href="./admin/user/create" class="btn-shadow btn-hover-shine mr-3 btn btn-primary">
                                     <span class="btn-icon-wrapper pr-2 opacity-7">
                                         <i class="fa fa-plus fa-w-20"></i>
                                     </span>
@@ -104,7 +104,9 @@
                                                             <i class="fa fa-edit fa-w-20"></i>
                                                         </span>
                                                     </a>
-                                                    <form class="d-inline" action="" method="post">
+                                                    <form class="d-inline" action="./admin/user/{{$user->id}}" method="post">
+                                                        @csrf
+                                                        @method("DELETE")
                                                         <button class="btn btn-hover-shine btn-outline-danger border-0 btn-sm"
                                                             type="submit" data-toggle="tooltip" title="Delete"
                                                             data-placement="bottom"
