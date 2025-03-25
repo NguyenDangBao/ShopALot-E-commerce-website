8ffd731ad6fc2eed3e29d0732c8deb838fa06bbd
NguyenDangBao <91870844+NguyenDangBao@users.noreply.github.com>
2025-03-25 14:22:08 +0700

    thêm CheckAdminLogin

---
+++ app/Http/Middleware/CheckAdminLogin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Utilities\Constant;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdminLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //Nếu chưa đăng nhập: chuyển hướng tới trang đăng nhập
+        if(Auth::guest()){
+            return redirect()->guest('admin/login');
+
+        }
+        //nếu đã đăng nhập, nhưng sai level: đăng xuất và đăng nhập lại
+        if(Auth::user()->level != Constant::user_level_host && Auth::user()->level != Constant::user_level_admin){
+            Auth::logout();
+            return redirect() ->guest('admin/login');
+        }
+        return $next($request);
+    }
+}
