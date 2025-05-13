028ed0ad49f5f8a12070eaf0e2ea048b6bc2441a
NguyenDangBao <91870844+NguyenDangBao@users.noreply.github.com>
2025-05-13 16:25:54 +0700

    Add files via upload

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Blog extends Model
 {
@@ -11,7 +12,20 @@ class Blog extends Model
     protected $table = 'blogs';
     protected $primaryKey = 'id';
     protected $guarded = [];
+    protected $fillable = [
+        'user_id',
+        'title',
+        'subtitle',
+        'image',
+        'category',
+        'content',
+    ];
     public function blogComments(){
         return $this->hasMany(BlogComment::class, 'blog_id', "id");
+    }
+    public function getSlugAttribute()
+    {
+        // Generate a slug from the title if one doesn't already exist
+        return Str::slug($this->title);
     }
 }
